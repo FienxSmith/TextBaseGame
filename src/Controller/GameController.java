b465e6446209c2d6592473a0c31a07f7e160c96a
Katrina Smith <ksmith10@ggc.edu>
2017-04-21 01:09:58 -0400

    Update

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -2,44 +2,27 @@ package Controller;
 
 import Model.*;
 import View.ViewConsole;
+
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 
 public class GameController {
-	
-	private String currentRoom;
-	private Rm room;
-	
-	public GameController() {
-		
-	}
-	
-	public GameController(Rm room, String currentRoom) {
-		this.room = room;
-		this.currentRoom = currentRoom;
-	}
-	
-	public String getCurrentRoom() {
-	
-		return currentRoom;
-	}
 
-	public void setCurrentRoom(String currentRoom) {
-		this.currentRoom = currentRoom;
-	}
-	
-	public Rm getRoom() {
-		return room;
-	}
-
-	public void setRoom(Rm room) {
-		this.room = room;
-	}
 
 	public static void main(String[] args) {
-		Rm room = new Rm("RM101"); //start of the game
-		GameController status = new GameController(room, "RM101");
+		
 		ViewConsole view = new ViewConsole();
+		Rm room = new Rm();
+		LoadRooms loadRmList = new LoadRooms();
+		HashMap<String, Rm> roomInventory = new HashMap<String, Rm>();
+		roomInventory = loadRmList.loadRooms("roomObjects.txt");
+		Scanner input = new Scanner(System.in);
+		String currentRoom;
+		String str = "";
 		
 		// Title Screen
 		System.out.println("+--------------------------------------------------------+");
@@ -50,17 +33,10 @@ public class GameController {
 		System.out.println("\t[1]  Start New Game");
 		System.out.println("\t[2]  Load Saved Game");
 
-		Scanner input = new Scanner(System.in);
-		String currentRoom = room.getRmID() + " --> " + room.getRmName();
-		String str = "";
-
 		str = input.nextLine();
 		if(str.equals("1")) 
-		{
-			String newGameStr = "You have just entered the Mansion. You are are currently in:\n\t" + status.getCurrentRoom() + ":\t"
-								+ room.getRmDescription(currentRoom);
-			String newStr = "\nHere are your Menu of Options";
-			view.print(newGameStr + "\n" + newStr + "\n");	
+		{	
+				System.out.println(roomInventory.get("RM101"));
 		}
 		else if (str.equals("2"))
 		{
@@ -75,24 +51,23 @@ public class GameController {
 				//View Commands
 				System.out.println("Please CHOOSE ONE of the following Menu of Options");
 				System.out.println("\t> TYPE IN THE NUMBER ONLY");
-				System.out.println("\t[1]  Enter Room");
+				System.out.println("\t[1]  Select Room");
 				System.out.println("\t[2]  Equipt an Item");
 				System.out.println("\t[3]  View Inventory");
 				System.out.println("\t[4]  Check Health Status");
 				System.out.println("\t[5]  Save Game");
 				System.out.println("\t[6]  Exit Game");
+				
 				String optionStr = input.nextLine();
 				String newStr = "";
+				
 				switch(optionStr) {
 				case "1": 
 					//enter room code
-					//currentRoom = room.getRmID() + " --> " + room.getRmName();
-					//newStr = "You have entered the Room zone\n" + "Your currently are on level " + room.getRmLevel(currentRoom.substring(0, 1)) +
-					//		". " + currentRoom;
 					newStr = "You have entered the Room zone\n";
 					view.print(newStr);
+
 					room.printRoomDetails();
-					
 					break;
 				/*case "2":
 					//attack monster code
@@ -134,6 +109,6 @@ public class GameController {
 				}
 			}
 	}
-}
 
+}
 
